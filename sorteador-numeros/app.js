862efda61c769bc18e6b8f58dab468bf1cda8b7d
Alvaro Alm3ida <alvarosantosalmeida120@gmail.com>
2025-01-16 21:43:11 -0300

    commit inicial

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear(){
+let quantidade = parseInt(document.getElementById('quantidade').value); // ao colocar na variável .value significa que vai ser pego o que esta no ID e trazer como valor não string
+let de = parseInt(document.getElementById('de').value); 
+let ate = ParseInt(document.getElementById('ate').value); 
+
+alert(`Quantidade: ${quantidade}`);
+alert(`de: ${de}`);
+alert(`Até: ${ate}`);
+
+}
