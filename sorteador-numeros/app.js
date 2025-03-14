b10dcc2ca62eab6f83b93adca03bad2063247a32
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-03-14 00:26:56 -0300

    alteração no sorteador-de-numeros

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,10 +1,27 @@
 function sortear(){
 let quantidade = parseInt(document.getElementById('quantidade').value); // ao colocar na variável .value significa que vai ser pego o que esta no ID e trazer como valor não string
 let de = parseInt(document.getElementById('de').value); 
-let ate = ParseInt(document.getElementById('ate').value); 
+let ate = parseInt(document.getElementById('ate').value); 
 
-alert(`Quantidade: ${quantidade}`);
-alert(`de: ${de}`);
-alert(`Até: ${ate}`);
+let sorteados = [];
+let numero;
+
+for (let i = 0; i < quantidade; i++){
+numero = obterNumeroAleatorio(de,ate);
+
+while (sorteados.includes(numero)){
+    numero = obterNumeroAleatorio(de,ate);
+}
+
+sorteados.push(numero);
+}
+
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
+
+}
+
+function obterNumeroAleatorio(min,max){
+return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
