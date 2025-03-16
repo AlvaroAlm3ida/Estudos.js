444e76728e747e6d26402a201baea43523cf67f4
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-03-16 00:53:37 -0300

    término do sorteador-de-numeros e inicio do alugames

---
+++ alugames/js/app.js
@@ -0,0 +1,13 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+   // let nameGame = gameClicado.querySelector('.dashboard__item__name');
+
+    //alert(nameGame.textContent); TESTE
+
+}
+
+
+
+
