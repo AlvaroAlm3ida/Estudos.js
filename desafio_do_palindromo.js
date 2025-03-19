fa7b5a0b77dce35df7f3bd864180a8182f309698
AlvaroAlm3ida <alvarosantosalmeida120@gmail.com>
2025-03-19 00:16:01 -0300

    fuction declaration x funcion express

--- desafio_do_palindromo.js
+++ desafio_do_palindromo.js
@@ -1,13 +1,31 @@
+//EXEMPLO DE FUNCION DECLARATION
 function essaPalavraEUmPalindromo () {
     var palavra = "rever";
     var separandoAsLetraDaPalavra = palavra.split("") // função do js para quebrar as palavras em uma lista de arrays
 
-    console.log(separandoAsLetraDaPalavra);
+   // console.log(separandoAsLetraDaPalavra);
     var palavraInvertida = separandoAsLetraDaPalavra.reverse();
+    palavraInvertida = palavraInvertida.join("");
+    if(palavra==palavraInvertida){
+        console.log("A palavra "+ palavra + " é um palídromo!");
+    }else{ 
+        console.log("A palavra "+ palavra + "não é um palíndromo!");
+    }
+} ;
+essaPalavraEUmPalindromo();
+
+// EXEMPLO DE FUNCION EXPRESS
+/*    const essaPalavraEUmPalindromo = function () {
+    var palavra = "rever";
+    var separandoAsLetraDaPalavra = palavra.split("") // função do js para quebrar as palavras em uma lista de arrays
+    var palavraInvertida = separandoAsLetraDaPalavra.reverse();
+
+    //console.log(separandoAsLetraDaPalavra);
+   
     palavraInvertida = palavraInvertida.join("");
     if(palavra==palavraInvertida){
         console.log("A palavra"+ palavra + "É um palídromo!");
     }else{ 
         console.log("A palavra"+ palavra + "não é um palíndromo!");
     }
-}
+} */
